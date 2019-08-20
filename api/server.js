0b2b0113ca2a3fdde2c999252568287475562349
Melanie Harris <mell.j.harris@gmail.com>
2019-08-19 23:03:10 -0700

    MVP

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const helmet = require('helmet')
 const cors = require('cors');
-const bcrypt = require('bcryptjs')
+
+const router = require('../users/router')
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use('/api', router)
 
-module.exports = server;
+module.exports = server; 
--- users/router.js
+++ users/router.js
@@ -4,9 +4,9 @@ const db = require('./model.js');
 const bcrypt = require('bcryptjs')
 const router = express.Router();
 
-router.post('/registration', (req, res)=>{
+router.post('/reg', (req, res)=>{
     const creds = req.body
-    const hash = bcrypt.hashSync(creds.password, 20) 
+    const hash = bcrypt.hashSync(creds.password, 14) 
     creds.password = hash
     return db.add(creds)
     .then(user=>{
@@ -16,7 +16,7 @@ router.post('/registration', (req, res)=>{
     })
 })
 
-router.post('/login', (req, res)=>{
+router.post('/login', restricted, (req, res)=>{
     let { password, username} = req.body
 
     db.findBy({username})
@@ -31,3 +31,30 @@ router.post('/login', (req, res)=>{
         res.status(500).json({message: 'you messed up, login failed'})
     })
 })
+
+//protection
+function restricted(req, res, next) {
+    // we'll read the username and password from headers
+    // the client is responsible for setting those headers
+    const { username, password } = req.headers;
+
+    // no point on querying the database if the headers are not present
+    if (username && password) {
+        Users.findBy({ username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'Invalid Credentials' });
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Unexpected error' });
+            });
+    } else {
+        res.status(400).json({ message: 'No credentials provided' });
+    }
+}
+
+module.exports= router;
